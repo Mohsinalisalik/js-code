ae4d6cbc5dd0ae713b797823593099e4d2867fe3
Mohsinalisalik <157796838+Mohsinalisalik@users.noreply.github.com>
2024-12-04 14:01:52 +0000

    day1 done

---
+++ 01_day/variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountEmail = "Mohsin2002@gmail.com";
+var accountName = "Mohsin Ali salik";
+
+accountEmail = "Mohsin2003@gmail.com";
+accountName = "Mohsin Al4 salik";
+
+console.table([accountEmail,accountId,accountName])
+
+
