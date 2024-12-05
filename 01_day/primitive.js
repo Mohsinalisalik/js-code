f0288d949e7fdbd0b59cf8e28ec334d1dc117e82
Mohsinalisalik <157796838+Mohsinalisalik@users.noreply.github.com>
2024-12-05 15:21:12 +0000

    primitive

---
+++ 01_day/primitive.js
@@ -0,0 +1,11 @@
+// 7 types of primitive 
+// string number null bool undefine symbol bigint
+
+//************************* 
+// reference type(non primitive)
+// Array object  function
+
+let myFunction = function(){
+    console.log("Hello world");
+}
+console.log(myFunction());
