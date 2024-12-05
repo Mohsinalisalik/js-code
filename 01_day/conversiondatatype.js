fe7af82be0f58167465e7803373c992f21fdb6e0
Mohsinalisalik <157796838+Mohsinalisalik@users.noreply.github.com>
2024-12-05 12:38:18 +0000

    conversionofDatatype

---
+++ 01_day/conversiondatatype.js
@@ -0,0 +1,17 @@
+let score = true;
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let isLoggedIn = "5";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn)
+
+//conversion in number 
+// "33" => 33
+// "33a" => NaN
+// true => 1
+// null => 0
