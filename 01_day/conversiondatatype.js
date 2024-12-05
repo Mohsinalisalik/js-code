06faccb703ad985cb4c444610b724a48c2d1cf26
Mohsinalisalik <157796838+Mohsinalisalik@users.noreply.github.com>
2024-12-05 13:11:36 +0000

    conversion and operators

--- 01_day/conversiondatatype.js
+++ 01_day/conversiondatatype.js
@@ -1,17 +1,36 @@
 let score = true;
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 let isLoggedIn = "5";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn)
 
 //conversion in number 
 // "33" => 33
 // "33a" => NaN
 // true => 1
 // null => 0
+// ***********************Operation*****************
+
+let value = 5;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+let gameCounter = 100;
+console.log(++gameCounter);
+
+
+
+
+
