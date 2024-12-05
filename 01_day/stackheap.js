43c889b18066fb3e643ad6b7390c3e3dc925ee09
Mohsinalisalik <157796838+Mohsinalisalik@users.noreply.github.com>
2024-12-05 16:58:02 +0000

    heapandstack

---
+++ 01_day/stackheap.js
@@ -0,0 +1,15 @@
+let name  = "Mohsin Ali salik";
+
+newName = name;
+newName = "qasim Ali Khan";
+console.log(newName)
+console.log(name)
+
+let userOne = {
+    email : "mohsinalisalik2002@gmail.com",
+    cnic :3434213,
+}
+let userTwo = userOne;
+userTwo.email = 'qasim';
+console.log(userOne);
+
