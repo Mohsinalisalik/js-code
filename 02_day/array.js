3ebcc90b9a1599ad7534154e009f34aa0b8308f0
Mohsinalisalik <157796838+Mohsinalisalik@users.noreply.github.com>
2024-12-07 18:45:49 +0000

    array1

---
+++ 02_day/array.js
@@ -0,0 +1,19 @@
+const cart = ['laptop','Mouse'];
+
+cart.push('keyboad','monitor')
+console.log(cart);
+
+cart.pop('monitor');
+console.log(cart);
+//push
+//pop
+//shift
+//unshift
+//include answer true or false
+//indexof -1 return when value not found
+//join type is string
+const myNumbers =[0,1,2,3,4,5,6]
+console.log(myNumbers.slice(3,5));
+console.log(myNumbers);
+
+
