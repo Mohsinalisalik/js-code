afa9257fa6e066b5f6a88a24cd5dfe74df1f513d
Mohsinalisalik <157796838+Mohsinalisalik@users.noreply.github.com>
2024-12-07 17:25:01 +0000

    array

---
+++ 01_day/date.js
@@ -0,0 +1,14 @@
+// let myDate = new Date();
+// console.log(myDate.toLocaleDateString());
+
+//  let myDate = new Date();
+// console.log(myDate.getDay());
+
+let timeStamp = new Date();
+
+console.log(timeStamp.toLocaleString('default',{
+    weekday : "long",
+    
+    
+}));
+
---
+++ 01_day/numberMath.js
@@ -0,0 +1,7 @@
+let score = 300;
+
+let gameScore = new Number(2000000000);
+
+console.log(gameScore.toPrecision());
+console.log(gameScore.toLocaleString('en-IN'));
+
